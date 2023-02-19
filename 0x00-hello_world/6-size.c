6104e00bfd82612efa8bce39825207cfa91fbed0
Emmanuel Vuvor <emmanuelvuvor@gmail.com>
2023-02-19 03:27:15 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,8 +7,8 @@ int main(void)
 {
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
 	printf("Size of a char: %i byte(s)\n", sizeof(int));
-	printf("Size of a char: %i byte(s)\n", sizeof(long int);
-			printf("Size of a char: %i byte(s)\n", sizeof(long long int));
-			printf("Size of a char: %i byte(s)\n", sizeof(float));
-			return (0);
-			}
+	printf("Sie of a char: %i byte(s)\n", sizeof(long int));
+	printf("Size of a char: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a char: %i byte(s)\n", sizeof(float));
+	return (0);
+}
