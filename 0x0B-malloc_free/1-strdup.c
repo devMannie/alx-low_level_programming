fe0dc4cd2928e60a1ae3deacde16376a726dc515
Emmanuel Vuvor <emmanuelvuvor@gmail.com>
2023-03-14 23:00:33 -0700

    4-free_grid.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
-* _strdup - duplicate to new memory space location
-* @str: char
-* Return: 0
-*/
+ * _strdup - duplicates a string to a new memory space location
+ * @str: pointer to the string to duplicate
+ *
+ * Return: pointer to the duplicated string, NULL if fail
+ */
 char *_strdup(char *str)
 {
-	char *aaa;
-	int i, r = 0;
+	char *new_str;
+	int i, len = 0;
 
 	if (str == NULL)
 		return (NULL);
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	aaa = malloc(sizeof(char) * (i + 1));
-	if (aaa == NULL)
-		return (NULL);
-	for (r = 0; str[r]; r++)
-		aaa[r] = str[r];
-	return (aaa);
 
+	while (str[len] != '\0')
+		len++;
+
+	new_str = malloc(sizeof(char) * (len + 1));
+
+	if (new_str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		new_str[i] = str[i];
+
+	new_str[i] = '\0';
+
+	return (new_str);
+}
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,49 +1,49 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
-* str_concat - get ends of input and add together for size
-* @s1: input one to concat
-* @s2: input two to concat
-* Return: concat of s1 and s2
-*/
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: pointer to concatenated string, NULL if fail
+ */
 char *str_concat(char *s1, char *s2)
 {
-	char *conct;
-	int i, ci;
+	char *concat;
+	int i, j;
+
 	if (s1 == NULL)
-
 		s1 = "";
-
 	if (s2 == NULL)
-
 		s2 = "";
 
-		i = ci = 0;
-
+	i = j = 0;
 	while (s1[i] != '\0')
 		i++;
+	while (s2[j] != '\0')
+		j++;
 
-	while (s2[ci] != '\0')
-		ci++;
+	concat = malloc(sizeof(char) * (i + j + 1));
 
-	conct = malloc(sizeof(char) * (i + ci + 1));
-
-	if (conct == NULL)
+	if (concat == NULL)
 		return (NULL);
-	i = ci = 0;
 
+	i = j = 0;
 	while (s1[i] != '\0')
 	{
-		conct[i] = s1[i];
+		concat[i] = s1[i];
 		i++;
 	}
 
-
-	while (s2[ci] != '\0')
+	while (s2[j] != '\0')
 	{
-		conct[i] = s2[ci];
-		i++, ci++;
+		concat[i] = s2[j];
+		i++;
+		j++;
 	}
-	conct[i] = '\0';
-	return (conct);
 
+	concat[i] = '\0';
+
+	return (concat);
+}
